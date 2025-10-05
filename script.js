23ba54de70b04674c463328f3c01a59e5a008744
Yordanos Shanbel <yordanosshanbel28@gmail.com>
2025-10-05 21:13:33 +0300

    add responsiveness

---
+++ script.js
@@ -0,0 +1,67 @@
+let ruleClose = document.querySelector('.rules__close');
+let overlay = document.querySelector('.overlay');
+let choiceContainer = document.querySelector('.choice-container');
+let subPick = document.querySelectorAll('.sub-pick');
+let pickedContainer = document.querySelector('.picked-container');
+let choices = choiceContainer.querySelectorAll('.choice-wrapper');
+let resultMessage = document.querySelector('.result');
+let playAgain = document.querySelector('.btn');
+let rulesOpen = document.querySelector('.one__rules');
+let score = document.querySelector('.score--value');
+
+ruleClose.addEventListener('click', function () {
+  overlay.style.display = 'none';
+});
+
+rulesOpen.addEventListener('click', function () {
+  overlay.style.display = 'block';
+});
+
+choices.forEach(function (choice) {
+  choice.addEventListener('click', initialChoice);
+});
+
+playAgain.addEventListener('click', function () {
+  choiceContainer.style.display = 'grid';
+  pickedContainer.style.display = 'none';
+  resultMessage.innerHTML = "";
+});
+
+function initialChoice() {
+  let randomNumber = Math.floor(Math.random() * choices.length);
+  let computerChoice = choices[randomNumber];
+  let userChoice = this;
+
+  choiceContainer.style.display = 'none';
+  pickedContainer.style.display = 'flex';
+
+  subPick[0].innerHTML = "<p class='user-picked-text'>YOU PICKED</p>";
+  subPick[0].appendChild(userChoice.cloneNode(true));
+
+  subPick[1].innerHTML = "<p class='computer-picked-text'>THE HOUSE PICKED</p>";
+  subPick[1].appendChild(computerChoice.cloneNode(true));
+
+  getWinner(computerChoice, userChoice);
+}
+
+let userScore = '0';
+function getWinner(computerChoice, userChoice) {
+  const user = userChoice.dataset.value;
+  const computer = computerChoice.dataset.value;
+
+  if (user === computer) {
+    resultMessage.innerHTML = "It's a draw!";
+  } else if (
+    (user === "rock" && computer === "scissors") ||
+    (user === "paper" && computer === "rock") ||
+    (user === "scissors" && computer === "paper")
+  ) {
+    resultMessage.innerHTML = "YOU WIN";
+    userScore++;
+    score.innerHTML = userScore;
+  } else {
+    resultMessage.innerHTML = "YOU LOSE";
+    userScore--;
+    score.innerHTML = userScore;
+  }
+}
